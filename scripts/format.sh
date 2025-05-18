d5368103f7217846c55068ffc3d1ee07b9526c49
BubbleFish <daforsastudia@gmail.com>
2025-05-18 21:37:35 +0300

    chore: Make formatting enforce consistent header and run async

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,32 @@
-dir=$(basename $(pwd))
+#!/usr/bin/bash
 
-if ! [ dir = "scripts" ]; then
-    cd scripts
+dir=$(basename $(pwd))
+header='<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="@color">'
+
+if [ dir = "scripts" ]; then
+    cd ..
 fi
 
-prettier --config .prettierrc.json --parser html --write "../icons/**/*.svg"
+cp -r icons/ /tmp/eyecons.bak
+
+function myformat {
+    file=$1
+    base=$(basename $file)
+
+    cp $file /tmp/$base.backup
+    echo $header > $file
+    tail -n+2 /tmp/$base.backup >> $file
+
+    prettier --log-level silent --config scripts/.prettierrc.json --parser html --write $file
+    echo "--- Formatted $base"
+}
+
+shopt -s globstar
+
+for file in icons/**/*.svg; do
+    myformat $file &
+done
+
+wait
+
+# prettier --config .prettierrc.json --parser html --write "../icons/**/*.svg"
